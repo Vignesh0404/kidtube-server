afd276ebbf437dba336b23c3da5cf876eb5ae830
vignesh0404 <vignesh6901@gmail.com>
2023-11-28 21:45:53 -0600

    cookies test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,15 +2,15 @@ const jwt = require("jsonwebtoken");
 
 const protect = async (req, res, next) => {
   try {
-    // const token = req.cookies.accessToken;
+    const token = req.cookies.accessToken;
     const cookies = res.get('Set-Cookie');
     const tokenCookie = cookies.find(cookie => cookie.startsWith('accessToken='));
-    console.log('token', tokenCookie)
-    if (!tokenCookie) {
+    console.log('token', token)
+    if (!token) {
       res.status(401);
       return next(new Error("Not authorized, no token"));
     }
-    const token = tokenCookie.split('=')[1];
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = { _id: decoded._id };
